efd9d0c4f7ee736cd780d23e0414eb9cfb8ab339
Jeremy BOURDIOL <jerem.dante@gmail.com>
2014-02-11 19:17:18 +0100

    UpdateMove
    Le move marche en faisant 1 D avec la reco de geste, je push le dico
    plus tard

--- src/fusionengineb/b/FusionEngineBB.java
+++ src/fusionengineb/b/FusionEngineBB.java
@@ -18,11 +18,15 @@ public class FusionEngineBB {
     private static final Logger LOG = Logger.getLogger(FusionEngineBB.class.getName());
     private final Ivy bus;
     private String lastSelected;
+    private boolean waiting_loc = false;
     private ArrayList<ArrayList<Point2D>> traces;
     
     private String x = "0";
     private String y = "0";
     private String obj = "";
+    private String x_last = "";
+    private String y_last = "";
+
     
     public FusionEngineBB() {
         traces = new ArrayList<>();
@@ -49,20 +53,41 @@ public class FusionEngineBB {
             bus.bindMsg("^Palette:ResultatTesterPoint x=([0-9]*) y=([0-9]*) nom=(.*)",new IvyMessageListener() {
                 public void receive(IvyClient client, String[] args) {
                     lastSelected = args[2];
+                    
                 }
             });
             bus.bindMsg("^Palette:MouseReleased x=([0-9]*) y=([0-9]*)",new IvyMessageListener() {
                 public void receive(IvyClient client, String[] args) {
                     try {
                         //bus.sendMsg("J'ai :"+args[0] + " : "+ args[1]);
+                        if(!waiting_loc) {
+                            bus.sendMsg("Palette:TesterPoint x="+args[0]+" y="+args[1]);
+                        }
+                        else {
+                            int tmpx = Integer.parseInt(args[0]) - (Integer.parseInt(x) + 40);
+                            int tmpy = Integer.parseInt(args[1]) - (Integer.parseInt(y) + 20);
+                            bus.sendMsg("Palette:DeplacerObjet nom="+lastSelected+" x="+tmpx+" y="+tmpy);
+                            waiting_loc = false;
+                        }
                         x = ""+(Integer.parseInt(args[0])-40);
                         y = ""+(Integer.parseInt(args[1])-20);
-                        bus.sendMsg("Palette:TesterPoint x="+args[0]+" y="+args[1]);
                     } catch (IvyException ex) {
                         Logger.getLogger(FusionEngineBB.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
             });
+            /*
+            //"Palette:Info nom=E2 x=79 y=68 longueur=100 hauteur=50 couleurFond=white couleurContour=black"
+            bus.bindMsg("^Palette:Info x=([A-Z]*[0-9]*) x=([0-9]*) y=([0-9]*)",new IvyMessageListener() {
+                public void receive(IvyClient client, String[] args) {
+                        //bus.sendMsg("J'ai :"+args[0] + " : "+ args[1]);
+                        x_last = ""+(Integer.parseInt(args[1])+40);
+                        y_last = ""+(Integer.parseInt(args[2])+20);
+                        System.out.println("Got x="+x+" y="+y);
+                }
+            });
+            */
+            
             bus.start(null);
         } catch (IvyException ex) {
             Logger.getLogger(FusionEngineBB.class.getName()).log(Level.SEVERE, null, ex);
@@ -90,6 +115,9 @@ public class FusionEngineBB {
                 break;
             case "Droite" :
                 rep = "DeplacerObjet nom="+lastSelected+" x=25 y=0";
+                break;
+            case "Deplacer" :
+                waiting_loc = true;
                 break;
         }
         return rep;
